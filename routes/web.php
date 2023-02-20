10dc02fbe89148c7b3806ce81bcf7f8f701c6bfd
Samuel <samuel@mknd-records.com>
2023-02-20 15:05:50 +0100

    clean laravel for futur exercices

--- routes/web.php
+++ routes/web.php
@@ -17,28 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/livret/{n}', function ($n) {
-    $resultat = [];
-    for ($i = 1; $i <= 12; $i++) {
-        $resultat[] = $i . " * " . $n . " = " . $i * $n;
-    }
-   echo implode("<br>", $resultat);
-
-})->where('n', '[1-9]|1[0-2]');
-
-Route::get('/{p}age1', function () {
-    return true;
-})->where('p', 'p|P');
-
-Route::get('/cff/{gare}/{time}/{direction}/{date?}', function($gare, $time, $direction, $date = null){
-    if($date == null){
-        $date = date('d.m.Y');
-    }
-    return redirect('https://www.sbb.ch/fr/acheter/pages/fahrplan/fahrplan.xhtml?von=' . $gare . '&zeit=' . $time . '&nach=' . $direction . '&suche=true&datum='. $date);
-});
-
-
-
 
 
 
